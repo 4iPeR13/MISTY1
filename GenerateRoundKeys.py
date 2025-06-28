c09de34a7c8369eedf064927f716ab975d50c65c
4iPeR13 <gleb.s.baranov@gmail.com>
2025-06-28 04:21:23 +0300

    Update GenerateRoundKeys.py

--- GenerateRoundKeys.py
+++ GenerateRoundKeys.py
@@ -9,25 +9,30 @@ KLiL = K(i+1)/2 (odd i) or K`(i/2)+2 (even i), KLiR = K` (i+1)/2+6 (odd i) or K(
 '''
 from functions import FI
 def rotate_index(i):
-    return i % 8
+    if i>8: 
+        return (i - 8)
+    else:
+        return i
 
 def GenerateRoundKeys(key: bytes) -> dict:
     assert len(key)==16
 
-    K = []
+    K = [0]
     for i in range(0, 16, 2):
           part = key[i:i+2]  
           number = int.from_bytes(part, byteorder='big') 
           K.append(number)
 
-    K_dash = []
-    for i in range(7):
-        K_dash.append(FI(K[i], K[i + 1]))
-    K_dash.append(FI(K[7], K[0]))
-   
+    K_dash = [0]
+    
+    for i in range(1, 8):
+        K_dash.append(FI(K[i], K[i+1]))
+    K_dash.append(FI(K[8], K[1]))
+ 
+
    
     KO = []
-    for i in range(8):
+    for i in range(1, 9):
         KO1 = K[rotate_index(i)]
         KO2 = K[rotate_index(i+2)]
         KO3 = K[rotate_index(i+7)] 
@@ -36,26 +41,28 @@ def GenerateRoundKeys(key: bytes) -> dict:
 
     
     KI = []
-    for i in range(8):
+    for i in range(1,9):
         KI1 = K_dash[rotate_index(i + 5)]
-        KI2 = K_dash[rotate_index(i + 1)]
+        KI2 = K_dash[rotate_index(i+1)]
         KI3 = K_dash[rotate_index(i + 3)]
         KI.append((KI1, KI2, KI3))
 
-    
     KL = []
-    for i in range(0, 10): 
+    for i in range(1, 11): 
         if i % 2 == 1: 
-            KL_L = K[((i + 1) // 2) % 8]
-            KL_R = K_dash[((i + 1) // 2 + 6 ) % 8]
+            # KL_L = K[((i + 1) // 2) % 8]
+            # KL_R = K_dash[((i + 1) // 2 + 6 ) % 8]
+            KL_L = K[rotate_index((i + 1) // 2)]
+            KL_R = K_dash[rotate_index((i + 1) // 2 + 6 )]
         else:  
-            KL_L = K_dash[(i // 2 + 2) % 8]
-            KL_R = K[(i // 2 + 4) % 8]
+            KL_L = K_dash[rotate_index(i // 2 + 2)]
+            KL_R = K[rotate_index(i // 2 + 4)]
         KL.append((KL_L, KL_R))
         
     return {
         "KO": KO,  # список з 8 кортежів (KO1, KO2, KO3, KO4)
         "KI": KI,  # список з 8 кортежів (KI1, KI2, KI3)
         "KL": KL,   # список з 10 кортежів (KL_L, KL_R)
-        "K_dash": K_dash}
+        # "K_dash": K_dash
+        }
     
