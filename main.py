13205d90b321783784a055eb8ee71d4355a4b45d
4iPeR13 <gleb.s.baranov@gmail.com>
2025-06-28 04:20:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from test import test
 from Encrypt import EncryptBlock
 from Decrypt import DecryptBlock
-
+from GenerateRoundKeys import GenerateRoundKeys
 
 if __name__ == "__main__":
     
     test()
 
-   
+    
     print("\n=== MISTY1 Шифрування ===")
     while True:
         print("\nОберіть дію:")
@@ -36,7 +36,14 @@ if __name__ == "__main__":
                 print("Помилка читання вхідного файлу:", e)
                 continue
             try:
-                enc = EncryptBlock(data, key_bytes)
+                key_data = GenerateRoundKeys(key_bytes)
+                pad_len = 8 - (len(data) % 8)
+                data += bytes([pad_len] * pad_len)
+
+                enc = b''
+                for i in range(0, len(data), 8):
+                    block = data[i:i+8]
+                    enc += EncryptBlock(block, key_data)
                 with open(outfile, 'wb') as f:
                     f.write(enc)
                 print(f"Файл зашифровано, результат записано в '{outfile}'.")
@@ -60,7 +67,15 @@ if __name__ == "__main__":
                 print("Помилка читання вхідного файлу:", e)
                 continue
             try:
-                dec = DecryptBlock(data, key_bytes)
+                key_data = GenerateRoundKeys(key_bytes)
+                dec = b''
+                for i in range(0, len(data), 8):
+                    block = data[i:i+8]
+                    dec += DecryptBlock(block, key_data)
+                pad_len = dec[-1]
+                if all(p == pad_len for p in dec[-pad_len:]):
+                    dec = dec[:-pad_len]
+
                 with open(outfile, 'wb') as f:
                     f.write(dec)
                 print(f"Файл розшифровано, результат записано в '{outfile}'.")
